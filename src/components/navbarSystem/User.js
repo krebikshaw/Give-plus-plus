1a1575cb671b678fa3dac98b06b4e8f85d8e7312
krebikshaw <krebikshaw@gmail.com>
2020-12-15 23:54:59 +0800

    add navbar icon hover demo

--- src/components/navbarSystem/User.js
+++ src/components/navbarSystem/User.js
@@ -6,14 +6,22 @@ import { COLOR, EFFECT } from '../../constants/style';
 
 const UserContainer = styled.div`
   position: relative;
+  &:hover {
+    & div {
+      opacity: 1;
+    }
+  }
 `;
 
 const OptionWrapper = styled.div`
   z-index: 1;
   display: block;
-  visibility: visible;
-  opacity: 1;
+
+  opacity: 0;
   transition: visibility 0s, opacity 0.2s linear;
+  position: absolute;
+  top: 23px;
+  right: 0;
   &::before {
     content: '';
     width: 0;
@@ -23,22 +31,33 @@ const OptionWrapper = styled.div`
     border-width: 0 7px 10px;
     position: absolute;
     top: 0;
-    right: 23px;
+    right: 13px;
     z-index: 7000;
+  }
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-color: transparent transparent #fff;
+    border-style: solid;
+    border-width: 0 6px 9px;
     position: absolute;
-    top: 36px;
-    right: 0;
-    color: #202026;
+    top: 1px;
+    right: 14px;
+    z-index: 7500;
   }
 `;
 
-const OptionList = styled.ul`
-  position: absolute;
-  background: ${COLOR.bg_primary};
-  box-shadow: ${EFFECT.shadowInput};
-  padding: 10px;
-  transform: translate(-30px, 5px);
+const OptionInner = styled.div`
+  padding-top: 9px;
   z-index: 1;
+  position: relative;
+  width: 100px;
+  background: ${COLOR.bg_primary};
+`;
+
+const OptionList = styled.ul`
+  border: 1px solid #e5e5e6;
 `;
 
 const OptionItem = styled.li``;
@@ -58,10 +77,12 @@ export default function User() {
     <UserContainer>
       <IconComponent kind={'user-circle'} />
       <OptionWrapper>
-        <OptionList>
-          {nickname && <OptionItem>{nickname}</OptionItem>}
-          <OptionItem>2</OptionItem>
-        </OptionList>
+        <OptionInner>
+          <OptionList>
+            {nickname && <OptionItem>{nickname}</OptionItem>}
+            <OptionItem>2</OptionItem>
+          </OptionList>
+        </OptionInner>
       </OptionWrapper>
     </UserContainer>
   );
