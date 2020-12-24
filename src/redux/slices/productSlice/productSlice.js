7134be39085937b7cd623e55d3320250fef0335b
small_leaf <hh741456963@gmail.com>
2020-12-25 00:22:19 +0800

    fix-getProductFromVendor

--- src/redux/slices/productSlice/productSlice.js
+++ src/redux/slices/productSlice/productSlice.js
@@ -115,7 +115,7 @@ export const getProductsFromVendor = (id, page, limit) => (dispatch) => {
   return getProductsFromVendorAPI(id, page, limit).then((res) => {
     if (res.ok === 0) {
       dispatch(setErrorMessage(res ? res.message : 'something wrong'));
-      return res.message;
+      return res.ok;
     }
     const { count, products } = res.data;
     dispatch(pushProducts(products));
