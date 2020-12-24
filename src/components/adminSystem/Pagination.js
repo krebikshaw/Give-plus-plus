d3c2768b75aeba24fae920f3f0ff772ec67ede50
krebikshaw <krebikshaw@gmail.com>
2020-12-24 11:56:08 +0800

    fix(admin): fix pagination bug

--- src/components/adminSystem/Pagination.js
+++ src/components/adminSystem/Pagination.js
@@ -18,6 +18,12 @@ export default function Pagination({ propsFunction, propsParams }) {
   const [totalPages, setTotalPages] = useState(1);
   const [limit] = useState(10);
 
+  const handleChangeCurrentPage = (page) => {
+    if (page === '<') return setCurrentPage((currentPage) => currentPage - 3);
+    if (page === '>') return setCurrentPage((currentPage) => currentPage + 3);
+    setCurrentPage(page);
+  };
+
   useEffect(() => {
     setTotalPages(Math.ceil(count / limit));
   }, [count]);
@@ -35,8 +41,38 @@ export default function Pagination({ propsFunction, propsParams }) {
   }, [currentPage]);
 
   useEffect(() => {
-    setPageList(Array.from({ length: totalPages }, (_, i) => i + 1));
+    if (totalPages <= 5)
+      setPageList(Array.from({ length: totalPages }, (_, i) => i + 1));
   }, [totalPages]);
+
+  useEffect(() => {
+    setPageList(() => {
+      if (totalPages <= 5)
+        return Array.from({ length: totalPages }, (_, i) => i + 1);
+      if (currentPage <= 3) return [1, 2, 3, 4, 5, '>', totalPages];
+      if (currentPage >= totalPages - 3)
+        return [
+          1,
+          '<',
+          totalPages - 4,
+          totalPages - 3,
+          totalPages - 2,
+          totalPages - 1,
+          totalPages,
+        ];
+      return [
+        1,
+        '<',
+        currentPage - 2,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        currentPage + 2,
+        '>',
+        totalPages,
+      ];
+    });
+  }, [currentPage, totalPages]);
 
   return (
     <PaginationContainer>
@@ -44,13 +80,16 @@ export default function Pagination({ propsFunction, propsParams }) {
         return page === currentPage ? (
           <NormalButton
             style={{ background: '#75e2d38c' }}
-            onClick={() => setCurrentPage(page)}
+            onClick={() => handleChangeCurrentPage(page)}
             key={index}
           >
             {page}
           </NormalButton>
         ) : (
-          <NormalButton onClick={() => setCurrentPage(page)} key={index}>
+          <NormalButton
+            onClick={() => handleChangeCurrentPage(page)}
+            key={index}
+          >
             {page}
           </NormalButton>
         );
