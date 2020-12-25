dec265f54ac83ae9eebe3cdd7930ff3d37f500ce
krebikshaw <krebikshaw@gmail.com>
2020-12-26 01:14:12 +0800

    build project

--- src/App.js
+++ src/App.js
@@ -59,57 +59,60 @@ const App = () => {
 
   return (
     <Root>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Navbar />
-          <Routes basename='/'>
-            <Route path={'/'} element={<HomePage />} />
-            <Route path={'/about'} element={<AboutUsPage />} />
-            <Route path={'/entrance'} element={<EntrancePage />} />
-            <Route path={'/login'} element={<LoginPage />} />
-            <Route path={'/register'} element={<RegisterPage />} />
-            <Route path={'/faq'} element={<FAQPage />} />
-            <Route path={'/contact'} element={<ContactUsPage />} />
-            <Route path={'/rules'} element={<RulesPage />} />
+      <GlobalStyle />
+      <BrowserRouter>
+        <Navbar />
+        <Routes basename='/Give-plus-plus'>
+          <Route path={'/'} element={<HomePage />} />
+          <Route path={'/about'} element={<AboutUsPage />} />
+          <Route path={'/entrance'} element={<EntrancePage />} />
+          <Route path={'/login'} element={<LoginPage />} />
+          <Route path={'/register'} element={<RegisterPage />} />
+          <Route path={'/faq'} element={<FAQPage />} />
+          <Route path={'/contact'} element={<ContactUsPage />} />
+          <Route path={'/rules'} element={<RulesPage />} />
 
-            <Route path={'/users'}>
-              <Route path={'/me'} element={<UserInfoPage />} />
-              <Route path={'/:id'} element={<UserInfoPage />} />
-              <Route path={'/apply'} element={<ApplyForVendorPage />} />
-              <Route path={'/backstage'} element={<VendorBackstagePage />} />
-              <Route path={'/contact/:id'} element={<VendorContactPage />} />
-              <Route path={'/vendor/:id'} element={<VendorInfoPage />} />
-            </Route>
+          <Route path={'/users'}>
+            <Route path={'/me'} element={<UserInfoPage />} />
+            <Route path={'/:id'} element={<UserInfoPage />} />
+            <Route path={'/apply'} element={<ApplyForVendorPage />} />
+            <Route path={'/backstage'} element={<VendorBackstagePage />} />
+            <Route path={'/contact/:id'} element={<VendorContactPage />} />
+            <Route path={'/vendor/:id'} element={<VendorInfoPage />} />
+          </Route>
 
-            <Route path={'/products'}>
-              <Route path={'/:id'} element={<ProductPage />} />
-              <Route path={'/edit/:id'} element={<EditProductPage />} />
-              <Route path={'/post'} element={<PostProductPage />} />
-              <Route path={'/search/:keyword'} element={<SearchProductPage />} />
-              <Route path={'/category/:id'} element={<CategorizedProductPage />} />
-              <Route path={'/vendor/:id'} element={<VendorShopPage />} />
-            </Route>
+          <Route path={'/products'}>
+            <Route path={'/:id'} element={<ProductPage />} />
+            <Route path={'/edit/:id'} element={<EditProductPage />} />
+            <Route path={'/post'} element={<PostProductPage />} />
+            <Route path={'/search/:keyword'} element={<SearchProductPage />} />
+            <Route
+              path={'/category/:id'}
+              element={<CategorizedProductPage />}
+            />
+            <Route path={'/vendor/:id'} element={<VendorShopPage />} />
+          </Route>
 
-            <Route path={'/cart'}>
-              <Route path={'/'} element={<CartPage />} />
-              <Route path={'/checkout'} element={<CheckoutPage />} />
-            </Route>
+          <Route path={'/cart'}>
+            <Route path={'/'} element={<CartPage />} />
+            <Route path={'/checkout'} element={<CheckoutPage />} />
+          </Route>
 
-            <Route path={'/orders'}>
-              <Route path={':id'} element={<OrderDetailPage />} />
-              <Route path={'/client'} element={<ClientOrdersPage />} />
-              <Route path={'/vendor'} element={<VendorOrdersPage />} />
-            </Route>
+          <Route path={'/orders'}>
+            <Route path={':id'} element={<OrderDetailPage />} />
+            <Route path={'/client'} element={<ClientOrdersPage />} />
+            <Route path={'/vendor'} element={<VendorOrdersPage />} />
+          </Route>
 
-            <Route path={'/admin'}>
-              <Route path={'/'} element={<AdminBackstagePage />} />
-              <Route path={'/users'} element={<AdminUserPage />} />
-              <Route path={'/products'} element={<AdminProductPage />} />
-              <Route path={'/mails'} element={<AdminMailPage />} />
-            </Route>
-          </Routes>
-          <Footer />
-        </BrowserRouter>
+          <Route path={'/admin'}>
+            <Route path={'/'} element={<AdminBackstagePage />} />
+            <Route path={'/users'} element={<AdminUserPage />} />
+            <Route path={'/products'} element={<AdminProductPage />} />
+            <Route path={'/mails'} element={<AdminMailPage />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </Root>
   );
 };
